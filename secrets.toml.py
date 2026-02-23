d7bf99800b1579dcf60f087f5fa37878f841af35
Simeon Stevenson TURAY (PhD) <127793300+ssturay@users.noreply.github.com>
2026-02-23 18:36:12 +0000

    Update secrets.toml.py

--- secrets.toml.py
+++ secrets.toml.py
@@ -1,17 +1,15 @@
-OPENCAGE_API_KEY = "your_actual_key_here"
-
-[credentials]
-username = "adminsst"
-password = "isst@2025"
-
 [google_credentials]
 type = "service_account"
-project_id = "xxxx"
-private_key_id = "xxxx"
-private_key = "-----BEGIN PRIVATE KEY-----\nXXXX\n-----END PRIVATE KEY-----\n"
-client_email = "xxxx@xxxx.iam.gserviceaccount.com"
-client_id = "xxxx"
+project_id = "your_project_id"
+private_key_id = "xxxxxxxxxxxxxxxx"
+private_key = """-----BEGIN PRIVATE KEY-----
+MIIEvQIBADANBgkqhkiG9w0BAQEFAASCBK...
+...many lines...
+...many lines...
+-----END PRIVATE KEY-----"""
+client_email = "xxxxx@xxxxx.iam.gserviceaccount.com"
+client_id = "xxxxxxxxxxxxxxxx"
 auth_uri = "https://accounts.google.com/o/oauth2/auth"
 token_uri = "https://oauth2.googleapis.com/token"
 auth_provider_x509_cert_url = "https://www.googleapis.com/oauth2/v1/certs"
-client_x509_cert_url = "xxxx"
+client_x509_cert_url = "https://www.googleapis.com/robot/v1/metadata/x509/xxxxx"
