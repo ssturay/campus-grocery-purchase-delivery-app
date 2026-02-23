ca519d373f3a919c6cc9044fe7bb72c1e203aa98
Simeon Stevenson TURAY (PhD) <127793300+ssturay@users.noreply.github.com>
2026-02-23 18:54:14 +0000

    Update secrets.toml.py

--- secrets.toml.py
+++ secrets.toml.py
@@ -1,15 +1,14 @@
-[google_credentials]
-type = "service_account"
-project_id = "your_project_id"
-private_key_id = "xxxxxxxxxxxxxxxx"
-private_key = """-----BEGIN PRIVATE KEY-----
-MIIEvQIBADANBgkqhkiG9w0BAQEFAASCBK...
-...many lines...
-...many lines...
------END PRIVATE KEY-----"""
-client_email = "xxxxx@xxxxx.iam.gserviceaccount.com"
-client_id = "xxxxxxxxxxxxxxxx"
-auth_uri = "https://accounts.google.com/o/oauth2/auth"
-token_uri = "https://oauth2.googleapis.com/token"
-auth_provider_x509_cert_url = "https://www.googleapis.com/oauth2/v1/certs"
-client_x509_cert_url = "https://www.googleapis.com/robot/v1/metadata/x509/xxxxx"
+GOOGLE_CREDENTIALS_JSON = """
+{
+  "type": "service_account",
+  "project_id": "your_project_id",
+  "private_key_id": "xxxx",
+  "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBg...\n-----END PRIVATE KEY-----\n",
+  "client_email": "xxxx@xxxx.iam.gserviceaccount.com",
+  "client_id": "xxxx",
+  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+  "token_uri": "https://oauth2.googleapis.com/token",
+  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/xxxx"
+}
+"""
