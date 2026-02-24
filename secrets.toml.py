d3a057532b73ebf9fbd683f956501b8c038c7e4b
Simeon Stevenson TURAY (PhD) <127793300+ssturay@users.noreply.github.com>
2026-02-24 13:22:43 +0000

    Update secrets.toml.py

--- secrets.toml.py
+++ secrets.toml.py
@@ -1,21 +1,19 @@
-# =========================
-# APP LOGIN CREDENTIALS
-# =========================
 [credentials]
-username = "adminsst"        # Replace with your app login username
-password = "isst@2025"       # Replace with your app login password
+username = "adminsst"
+password = "isst@2025"
 
-# =========================
-# GOOGLE SERVICE ACCOUNT
-# =========================
 [google]
-type = "service_account"
-project_id = "groc-purchase-and-deliveryapp"
-private_key_id = "f96135e39b56498527e6ac630bcd960a17e94771"
-private_key = "-----BEGIN PRIVATE KEY-----\\nMIIEvQIBADANBgkq...\\n-----END PRIVATE KEY-----\\n"
-client_email = "campusgroceryapp@groc-purchase-and-deliveryapp.iam.gserviceaccount.com"
-client_id = "114261537315232932947"
-auth_uri = "https://accounts.google.com/o/oauth2/auth"
-token_uri = "https://oauth2.googleapis.com/token"
-auth_provider_x509_cert_url = "https://www.googleapis.com/oauth2/v1/certs"
-client_x509_cert_url = "https://www.googleapis.com/robot/v1/metadata/x509/campusgroceryapp@groc-purchase-and-deliveryapp.iam.gserviceaccount.com"
+GOOGLE_CREDENTIALS_JSON = """
+{
+  "type": "service_account",
+  "project_id": "groc-purchase-and-deliveryapp",
+  "private_key_id": "NEW_PRIVATE_KEY_ID",
+  "private_key": "-----BEGIN PRIVATE KEY-----\\n...\\n-----END PRIVATE KEY-----\\n",
+  "client_email": "campusgroceryapp@groc-purchase-and-deliveryapp.iam.gserviceaccount.com",
+  "client_id": "NEW_CLIENT_ID",
+  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+  "token_uri": "https://oauth2.googleapis.com/token",
+  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/campusgroceryapp%40groc-purchase-and-deliveryapp.iam.gserviceaccount.com"
+}
+"""
