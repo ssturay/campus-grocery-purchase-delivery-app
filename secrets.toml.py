cbb05c98991518ee57529dd1dc8f19ddbd7c3b36
Simeon Stevenson TURAY (PhD) <127793300+ssturay@users.noreply.github.com>
2026-02-24 12:43:38 +0000

    Update secrets.toml.py

--- secrets.toml.py
+++ secrets.toml.py
@@ -10,8 +10,8 @@ password = "isst@2025"   # Replace with your app login password
 # =========================
 [google]
 type = "service_account"
-project_id = "your_project_id"  # Replace with your Google Cloud project ID
-private_key_id = "xxxx"         # Replace with your private key ID
+project_id = "groc-purchase-and-deliveryapp"  # Replace with your Google Cloud project ID
+private_key_id = "f96135e39b56498527e6ac630bcd960a17e94771"         # Replace with your private key ID
 private_key = "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkq...\n-----END PRIVATE KEY-----\n"  # Single-line with \n
 client_email = "campusgroceryapp@groc-purchase-and-deliveryapp.iam.gserviceaccount.com"  # Replace with your service account email
 client_id = "114261537315232932947"                                # Replace with your client ID
